868a1b2ec40329120a151f500b646a07858dcf65
octo <jamine61@mail.ru>
2025-02-09 02:21:15 +0300

    [NF] update styles

--- src/components/repo-card/RepoCard.tsx
+++ src/components/repo-card/RepoCard.tsx
@@ -16,12 +16,12 @@ export const RepoCard: FC<TRepoCardProps> = ({
   const dateFormatted = format(updated_at, 'dd MMMM uu', { locale: ru })
 
   return (
-    <div className="bg-slate-50 p-3.5">
+    <div className="bg-slate-50 p-3.5 m-1 ml-2 border-slate-100 border-2 text-slate-700">
       <div className="flex flex-row justify-between">
         <div className="text-3xl font-bold">{name}</div>
-        <div className="bg-center bg-no-repeat bg-star">{stargazers_count}</div>
+        <div className="bg-center bg-no-repeat bg-star ml-2">{stargazers_count}</div>
       </div>
-      <div>{description}</div>
+      <div className="text-slate-500 my-2 ml-1">{description}</div>
       <a href={html_url}>{html_url}</a>
       <div>
         <span>{formatMessage({ id: 'lastUpdate' })}</span>
--- src/components/search-input/SearchInput.tsx
+++ src/components/search-input/SearchInput.tsx
@@ -10,8 +10,20 @@ export const SearchInput: FC<TSearchInputPops> = ({ value, setValue }) => {
   }
 
   return <input
+    className={["text-3xl",
+      "placeholder:text-slate-300",
+      "px-2 py-1 m-2",
+      "bg-gradient-to-b",
+      "from-white",
+      "to-slate-50",
+      "rounded-md",
+      "border-slate-100",
+      "border-2",
+      "focus-visible:outline-slate-500"
+    ].join(' ')}
     value={value}
     onChange={changeHandler}
-    placeholder={formatMessage({ id: 'inputPlaceholder' })
-    }></input>
+    placeholder={
+      formatMessage({ id: 'inputPlaceholder' })
+    } ></input >
 }
