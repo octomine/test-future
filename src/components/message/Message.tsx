88a248b70b63325f1978625657e8106779581e27
octo <jamine61@mail.ru>
2025-02-10 11:51:32 +0300

    [NF] Message more better

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
 import { TMessageProps } from "./Message.types";
 
-export const Message: FC<TMessageProps> = ({ message }) => <div>{message}</div>
+export const Message: FC<TMessageProps> = ({ message }) =>
+  <div className="h-full flex justify-center items-center">{message}</div>
